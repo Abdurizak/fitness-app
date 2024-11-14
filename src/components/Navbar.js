0dd47003f6a637cb23c95d9fff44452c6d2b85e8
Abdurizak Abubakar <abdurizak.abubakar@student.moringaschool.com>
2024-11-14 21:19:49 +0300

    Add

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,6 +19,9 @@ const Navbar = () => {
             <li className="nav-item">
               <Link className="nav-link" to="/fitness">Fitness</Link>
             </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">Contact</Link>
+            </li>
           </ul>
         </div>
       </div>
